ae995c672d59271dc5ae91f86caeba4dafa6fa32
Chimaobi Wogu <woguchimaobi@gmail.com>
2020-12-31 00:52:35 +0100

    added dev file

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+    googleClientID: '288195988438-arh3qd6hc60pne24dmhniamd29d27r41.apps.googleusercontent.com',
+    googleClientSecret: '52Fz-ZbTZf2rAxsuUKGcpucj',
+    facebookAppID: '349828612564241',
+    facebookAppSecret: '0096992c551f836a21f3a006a8e74798',
+    mongoURI: 'mongodb+srv://CharlesJohnson:charlesjohnson@cluster0-mwtin.mongodb.net/surveybuddy-dev',
+    cookieKey: 'skdjsnfjksd23232k3m2k32k3n2k3232',
+    paystackSecretKey: 'sk_test_897fb1af416837e5cdfef615501d2f3835d1fd94',
+    email: 'chimaobi.dev@gmail.com',
+    password: 'marvis1995'
+  }
+  
